53f91db07904e9ba3b8cb60b3ee06ddf48c25761
Baldez Jr <abaldezjr@gmail.com>
2022-12-28 19:28:28 -0300

    atualizando a socialbar com react icons

--- frontend/src/components/SocialBar/index.js
+++ frontend/src/components/SocialBar/index.js
@@ -1,40 +1,36 @@
 import './style.css';
 
-import whatsapp from '../../assets/img/socialbar/whatsapp.svg';
-import twitter from '../../assets/img/socialbar/twitter.svg';
-import youtube from '../../assets/img/socialbar/youtube.svg';
-import facebook from '../../assets/img/socialbar/facebook.svg';
-import instagram from '../../assets/img/socialbar/instagram.svg';
+import { BsWhatsapp, BsTwitter, BsYoutube, BsFacebook, BsInstagram } from 'react-icons/bs';
 
 const Socialbar = () => {
     return (<>
         <ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
             <li className="ms-3">
-                <a className="text-muted" target="_blank" rel="noreferrer" href="https://api.whatsapp.com/send?phone=555391920073&text=Ol%C3%A1!">
-                    <img className="socialbar-redes" src={whatsapp} alt="whatsapp" />
+                <a className="link-dark" target="_blank" rel="noreferrer" href="https://api.whatsapp.com/send?phone=555391920073&text=Ol%C3%A1!">
+                    <BsWhatsapp className='socialbar-redes'/>
                 </a>
             </li>
             <li className="ms-3">
-                <a className="text-muted" target="_blank" rel="noreferrer" href="https://www.twitter.com/aresdenina/">
-                    <img className="socialbar-redes" src={twitter} alt="twitter" />
+                <a className="link-dark" target="_blank" rel="noreferrer" href="https://www.twitter.com/aresdenina/">
+                    <BsTwitter className='socialbar-redes'/>
                 </a>
             </li>
             <li className="ms-3">
-                <a className="text-muted" target="_blank" rel="noreferrer" href="https://www.youtube.com/aresdenina/">
-                    <img className="socialbar-redes" src={youtube} alt="youtube" />
+                <a className="link-dark" target="_blank" rel="noreferrer" href="https://www.youtube.com/aresdenina/">
+                    <BsYoutube className='socialbar-redes'/>
                 </a>
             </li>
             <li className="ms-3">
-                <a className="text-muted" target="_blank" rel="noreferrer" href="https://www.instagram.com/nauproducoes/">
-                    <img className="socialbar-redes" src={facebook} alt="facebook" />
+                <a className="link-dark" target="_blank" rel="noreferrer" href="https://www.instagram.com/nauproducoes/">
+                    <BsFacebook className='socialbar-redes'/>
                 </a>
             </li>
             <li className="ms-3">
-                <a className="text-muted" target="_blank" rel="noreferrer" href="https://www.instagram.com/nauproducoes/">
-                    <img className="socialbar-redes" src={instagram} alt="instagram" />
+                <a className="link-dark" target="_blank" rel="noreferrer" href="https://www.instagram.com/nauproducoes/">
+                    <BsInstagram className='socialbar-redes'/>
                 </a>
             </li>
-        </ul>
+        </ul >
     </>);
 }
 
